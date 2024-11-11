c095b854e52e251fa66ebf83b44a63d6aa5b0f7f
Guilherme Rezende <guillerplr@gmail.com>
2024-11-11 15:03:14 -0300

    letra é deselecionada ao segurar o botão

--- scrit.js
+++ scrit.js
@@ -1,6 +1,22 @@
-const botoes = document.querySelectorAll('li');
+const botoes = document.querySelectorAll("li");
 
+let holdTimer;
 
-botoes.forEach((e)=>{
-    e.addEventListener('click', (e)=> e.target.classList.add('selecionado'))
-})
+function handleMouseDown(e) {
+  e.target.classList.add("selecionado");
+  e.preventDefault();
+  holdTimer = setTimeout(function () {
+    e.target.classList.remove("selecionado");
+}, 1000);
+}
+
+function handleMouseUp(e) {
+  clearTimeout(holdTimer);
+}
+
+botoes.forEach((e) => {
+  e.addEventListener("mousedown", handleMouseDown);
+  e.addEventListener("mouseup", handleMouseUp);
+  e.addEventListener("touchstart", handleMouseDown);
+  e.addEventListener("touchend", handleMouseUp);
+});
